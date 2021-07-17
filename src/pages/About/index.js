4bab921d6f495cb0196db4e707e4c05313da3961
Sam Germain <sam.germain@usask.ca>
2021-07-16 18:22:38 -0600

    Added alts to images

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -16,14 +16,17 @@ export default () => (
     <Carousel className="mt-3">
       <img
         className="c-image"
+        alt="A cat"
         src="https://suddenlysask.com/static/cc701b5ed9329f7c62ace15b5dfec7e6/d00b9/cat.webp"
       />
       <img
         className="c-image"
+        alt="A bird"
         src="https://suddenlysask.com/static/cd1501538c7d4b269a999c6bb8521bdb/4efd5/blue-song-bird.webp"
       />
       <img
         className="c-image"
+        alt="Some dogs"
         src="https://suddenlysask.com/static/0690cbd4689de5b54db2cb4e26dfbd3f/d00b9/cover.webp"
       />
     </Carousel>
