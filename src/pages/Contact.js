bdb98023e80c3f9218b1e4faf52077b4212686c1
Sam Germain <sam.germain@usask.ca>
2021-07-29 23:35:10 -0600

    Changed page titles and desc

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Head, EmailForm } from "components";
 
 const metadata = {
-  description: "Web page description",
-  title: "Home",
+  description: "Contact page",
+  title: "Contact",
   shareImage:
     "https://suddenlysask.com/static/cc701b5ed9329f7c62ace15b5dfec7e6/d00b9/cat.webp",
-  path: "/",
+  path: "/contact",
 };
 
 export default () => (
