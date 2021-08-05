8c50e8b26dcc14a5ce0f1cd5fd7f75a79a32b489
Sam Germain <sam.germain@usask.ca>
2021-08-04 19:20:01 -0600

    Edited postcss.config to include purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
+const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
-    plugins: {
-        'cssnano': {}
-    }
-}
+  //   plugins: {
+  //     cssnano: {},
+  //   },
+  plugins: [
+    //Doesn't work, all css removed
+    // @ts-ignore
+    purgecss({
+      content: ["./src/**/*.js"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      safelist: {
+        //standard: [/fa-$/],
+        deep: [/.*fa-.*$/, /react-share__ShareButton/, /cont/],
+
+        //greedy: [/yellow$/],
+      },
+    }),
+  ],
+};
