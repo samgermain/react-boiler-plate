4626467551bded4dc1312369f2d8439d499a2b44
Sam Germain <sam.germain@usask.ca>
2021-08-10 16:39:33 -0600

    Removed excess margin classes, purgecss only on production

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 
-module.exports = {
-  //   plugins: {
-  //     cssnano: {},
-  //   },
+module.exports = ({env}) => ({
   plugins: [
     //Doesn't work, all css removed
+    require('cssnano')({
+      preset: 'default',
+    }),
     // @ts-ignore
-    purgecss({
+    env === 'production' && purgecss({
       content: ["./src/**/*.js"],
       defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
       safelist: {
@@ -18,4 +18,4 @@ module.exports = {
       },
     }),
   ],
-};
+});
