2d97982fb9e19e405eff690e7c5b05ed522d3ba4
Sam Germain <sam.germain@usask.ca>
2025-01-28 17:36:25 -0600

    updated image component with width and height params

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-export default ({ webp = "", other, alt, className = "", ...props }) => (
+export default ({ webp = "", other, alt, width = "", height = "", className = "", ...props }) => (
   <picture className={`d-inline-block ${className}`} {...props}>
     <source srcSet={webp} style={{ maxWidth: "100%" }} />
-    <img src={other} alt={alt} style={{ maxWidth: "100%" }} />
+    <img 
+      src={other}
+      alt={alt}
+      style={{ maxWidth: "100%" }}
+      width={width}
+      height={height}
+    />
   </picture>
 );
